f7d2d478320cbefcf2f690f7517db44c24f23660
ardila <146051568+putuardila@users.noreply.github.com>
2024-12-10 11:55:05 +0800

    10-12-2024

---
+++ OOP-B-MODUL-5/17-SubclassConstructor/src/com/tutorial/Main.java
@@ -0,0 +1,9 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        // membuat objek
+        HeroTank hero2 = new HeroTank("Balmod", 200, 100);
+        hero2.cetak();
+    }
+}
